2f8a0d1fdc7ddaa64ffa64d9143653953c261f41
GuillinJorge <jorgeguillin@hotmail.com>
2025-12-03 10:20:38 -0300

    Add Middleware

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+
+import mantenimiento from "./src/middlewares/mantenimiento.js";
+app.use (mantenimiento);
+
+
 app.get('/', (req, res) => {
-    res.send("Bienvenidos a nuestra API REST");
+    res.json({'message':"Bienvenidos a nuestra API REST"});
 });
 
+import notFound from "./src/middlewares/not-found.js";
+app.use(notFound);
 
 app.listen(PORT, ()=>{
     console.log(`http://localhost:${PORT}`)
